4203da62d85e49fa3d558a2f6aef1e6142c912f1
MD-Developer-yt <animeworldday2005@gmail.com>
2026-05-05 21:59:09 +0530

    config.py

---
+++ config.py
@@ -0,0 +1,27 @@
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
+# ------------------------- #
+
+import os
+
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+OWNER_ID = int(os.environ.get("OWNER_ID"))
+OWNER_NAME = os.environ.get("OWNER_NAME", "Owner")
+
+START_PIC = os.environ.get(
+    "START_PIC",
+    "https://graph.org/file/cb707ebcf6e087d4a49c6-ce0dbf8bc97b6dd50b.jpg"
+)
+
+DB_NAME = "bot.db"
+
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
+# ------------------------- #
